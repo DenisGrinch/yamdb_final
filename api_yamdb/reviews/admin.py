7a96d7584b2df79b9e5fcbf087f8596d4161e7b7
Denis Karpushin <grincharg@gmail.com>
2022-09-23 20:58:58 +0200

    commit three

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from reviews.models import Category, Genre, GenreTitle, Title
 
-
 admin.site.register(Title)
 admin.site.register(Genre)
 admin.site.register(Category)
